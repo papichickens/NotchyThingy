118a539b374ffecdc487b725ecc7a7c3d1122650
papichickens <102960100+papichickens@users.noreply.github.com>
2025-04-25 17:53:19 +0200

    Initial Commit

---
+++ NotchThingy/NotchThingyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchThingyApp.swift
+//  NotchThingy
+//
+//  Created by Nuno Alves on 25/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct NotchThingyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotchThingyTests/NotchThingyTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotchThingyTests.swift
+//  NotchThingyTests
+//
+//  Created by Nuno Alves on 25/04/2025.
+//
+
+import Testing
+@testable import NotchThingy
+
+struct NotchThingyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
